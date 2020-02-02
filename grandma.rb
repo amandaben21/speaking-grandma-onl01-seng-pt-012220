0e2250075ad16b0b43cd2d0f82cf4a97760d2612
Amanda Contreras <amandaben21@aol.com>
2020-02-02 01:27:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,8 @@ def speak_to_grandma (pharse)
   if pharse == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TO PUMPKIN!"
     elsif pharse == pharse.upcase
-    "NO,NOT SINCE 1938!"
+    "NO, NOT SINCE 1938!"
   else
-    "HUH?!SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   end
 end
